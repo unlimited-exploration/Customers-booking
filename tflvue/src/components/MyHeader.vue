613ac79bb736aa98878df1d8e12720de52410092
Unlimited-development <461136706@qq.com>
2019-08-15 21:54:54 +0800

    8.15

--- tflvue/src/components/MyHeader.vue
+++ tflvue/src/components/MyHeader.vue
@@ -1,1 +1,25 @@
 //导航条
+<template>
+  <header>
+        <div class="nav_header">
+            <ul class="clear">
+                <li>
+                    <img src="../img/icon_yytexa96bo/home.png">
+                    <a href="#">首页</a>
+                </li>
+                <li>
+                    <img src="../img/icon_yytexa96bo/liangcai.png">
+                    <a href="#">浏览菜单</a>
+                </li>
+                <li>
+                    <img src="../img/icon_yytexa96bo/user.png">
+                    <a href="#">我的账户</a>
+                </li>
+                <li>
+                    <img src="../img/icon_yytexa96bo/check.png">
+                    <a href="#">帮助</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
